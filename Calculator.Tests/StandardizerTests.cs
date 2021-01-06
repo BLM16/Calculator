aa2bbf3c8faa060476b0099243610cd384099477
Bradley Myers <myers.bradley@hotmail.com>
2021-01-06 18:49:20 -0500

    Fixed test asserting Arithmetic exception not BracketsError

--- Calculator.Tests/StandardizerTests.cs
+++ Calculator.Tests/StandardizerTests.cs
@@ -60,7 +60,7 @@ namespace Calculator.Tests
             }
 
             // Verify that having too many ')' throws an ArithmeticException
-            Assert.ThrowsException<ArithmeticException>(() => Standardizer.FixBrackets("(32*7)+4(3)+17)"));
+            Assert.ThrowsException<BracketsError>(() => Standardizer.FixBrackets("(32*7)+4(3)+17)"));
         }
 
         /// <summary>
--- Calculator/BracketsError.cs
+++ Calculator/BracketsError.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calculator
 {
-    class BracketsError : System.ApplicationException
+    public class BracketsError : System.ApplicationException
     {
         public BracketsError(string message) : base(message) { }
         public BracketsError(string message, System.Exception innerException) : base(message, innerException) { }
