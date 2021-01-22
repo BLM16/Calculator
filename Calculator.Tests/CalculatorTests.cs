60fe15f8caf745b51c3b6f8c01280daf5f23928d
Bradley Myers <myers.bradley@hotmail.com>
2021-01-22 15:54:28 -0500

    Added calculation functionality

---
+++ Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [DataTestMethod]
+        [DataRow("156(piroot(7))^2", "10777.608005989581")]
+        [DataRow("156(pi3)root(7+19)^2", "38226.8994088806")]
+        public void Calculate(string equation, string expected)
+        {
+            var actual = Calculator.Calculate(equation);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow("@((4+4)^2)", "8")]
+        [DataRow("@(@(32+9^2))", "3.260390438695134")]
+        [DataRow("@(@(@(81)))", "1.7320508075688772")]
+        [DataRow("@(16)*2", "8")]
+        public void SqrtTest(string equation, string expected)
+        {
+            var actual = Calculator.Solve(equation);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow("2^2^2", "16")]
+        [DataRow("3*3*3", "27")]
+        [DataRow("2+2+7", "11")]
+        public void DoublesTest(string equation, string expected)
+        {
+            var actual = Calculator.Calculate(equation);
+            Assert.AreEqual(expected, actual);
+        }
+
+        #region FindErrors
+
+        /// <summary>
+        /// Checks that illegal chars throw a MathSyntaxError.
+        /// </summary>
+        /// <param name="equation">Equation to test.</param>
+        [DataTestMethod]
+        [DataRow("32x+97*(34)")]
+        [DataRow("13^(7)+2!")]
+        [ExpectedException(typeof(MathSyntaxError))]
+        public void FindErrors_ExceptionOnIllegalChars(string equation)
+            => Calculator.FindErrors(equation);
+
+        /// <summary>
+        /// Checks that root signs without brackets wrapping their contents throws a MathSyntaxError.
+        /// </summary>
+        /// <param name="equation">Equation to test.</param>
+        [DataTestMethod]
+        [DataRow("13+@27")]
+        [DataRow("12*@")]
+        [DataRow("@")]
+        [ExpectedException(typeof(MathSyntaxError))]
+        public void FindErrors_ExceptionOnRootWithNoBrackets(string equation)
+            => Calculator.FindErrors(equation);
+
+        /// <summary>
+        /// Checks that having a non numeric value following a decimal throws a MathSyntaxError.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(MathSyntaxError))]
+        public void FindErrors_ExceptionOnNonNumericValueAfterDecimal()
+            => Calculator.FindErrors("32.+9");
+
+        #endregion
+
+        #region Solve
+
+        /// <summary>
+        /// Checks that equations containing brackets are solved correctly.
+        /// </summary>
+        /// <param name="equation">Equation to solve.</param>
+        /// <param name="expected">Expected result.</param>
+        [DataTestMethod]
+        [DataRow("2^2*(3^2*(4^2))", "576")]
+        [DataRow("2^2*(3^2+2^2)", "52")]
+        [DataRow("(2^3)+3^2*(2^3*(2^3+2^2))", "872")]
+        [DataRow("(132)", "132")]
+        public void Solve_BracketEquations(string equation, string expected)
+        {
+            var actual = Calculator.Solve(equation);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Checks that equations containing exponents are solved correctly.
+        /// </summary>
+        [TestMethod]
+        public void Solve_Exponents()
+        {
+            var equation = "3^5";
+            var expected = "243";
+
+            var actual = Calculator.Solve(equation);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Checks that equations containing square roots are solved correctly.
+        /// </summary>
+        [TestMethod]
+        public void Solve_SquareRoots()
+        {
+            var equation = "@(64)";
+            var expected = "8";
+
+            var actual = Calculator.Solve(equation);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Checks that equations containing division, multiplication, addition, or subtraction are solved correctly.
+        /// </summary>
+        /// <param name="equation">Equation to solve.</param>
+        /// <param name="expected">Expected result.</param>
+        [DataTestMethod]
+        [DataRow("81/9/3", "3")]
+        [DataRow("17*4*2", "136")]
+        [DataRow("22+117+11", "150")]
+        [DataRow("194-167-12", "15")]
+        public void Solve_DivisionMultiplicationAdditionSubtraction(string equation, string expected)
+        {
+            var actual = Calculator.Solve(equation);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Checks that division by zero throws a DivideByZeroException.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Solve_ExceptionOnDivisionByZero()
+        {
+            var equation = "321/0";
+            Calculator.Solve(equation);
+        }
+
+        /// <summary>
+        /// Checks that equations containing the pi '#' are converted and solved correctly.
+        /// </summary>
+        [TestMethod]
+        public void Solve_PiGetsConverted()
+        {
+            Assert.AreEqual("10.2", Calculator.Solve("3.2+7"));
+            Assert.AreEqual("9.42477796076938", Calculator.Solve("3*#"));
+        }
+
+        #endregion
+    }
+}
--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,4 +1,7 @@
-﻿namespace Calculator
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Calculator
 {
     /// <summary>
     /// Class calculates math in a string. Calls required methods in order.
@@ -14,6 +17,205 @@
         {
             // Standardize the string to optimize the Evaluator
             eq = Standardizer.Standardize(eq);
+
+            // Find any errors in the equation
+            // Invalid chars, operators, etc
+            FindErrors(eq);
+
+            // Solve the standardized equation
+            var res = Solve(eq);
+
+            return res;
+        }
+
+        /// <summary>
+        /// Finds any syntax errors that will break our Calculator otherwise.
+        /// </summary>
+        /// <param name="eq">Equation to check for errors in.</param>
+        public static void FindErrors(string eq)
+        {
+            if (eq.Length < 1)
+                throw new MathSyntaxError("Equation too short to be valid");
+
+            for (int i = 0; i < eq.Length; i++)
+            {
+                // Finds illegal chars
+                if (!"()^@#/*+-0123456789.".Contains(eq[i].ToString()))
+                    throw new MathSyntaxError($"Invalid char - @Ch:{i + 1}");
+
+                // Finds square root values that aren't wrapped in brackets
+                // Valid: @(x) - Invalid: @x - Invalid: @
+                if (eq[i] == '@' && (i + 1 >= eq.Length || (eq[i + 1] != '(')))
+                    throw new MathSyntaxError($"Square root contents must be wrapped in brackets - @Ch:{i + 1}");
+
+                // Finds decimal points that aren't followed by a number
+                // Valid: x.xx - Invalid: x. - Invalid: x./
+                if (eq[i] == '.' && (i + 1 > eq.Length || (!"0123456789".Contains(eq[i + 1].ToString()))))
+                    throw new MathSyntaxError($"Decimals must be followed by valid digits - @Ch:{i + 1}");
+            }
+        }
+
+        /// <summary>
+        /// Solves the math in a string following BEDMAS rules.
+        /// </summary>
+        /// <param name="eq">The equation to solve.</param>
+        /// <returns>The solved equation.</returns>
+        public static string Solve(string eq)
+        {
+            var bracketPattern = new Regex(@"(?<!@)\((?<eq>\d+(\.\d+)?([\^@/\*\+\-])?\d+([\^@/\*\+\-0-9.]+)?)\)");
+
+            // Convert the standardized pi symbol to Math.PI
+            eq = eq.Replace("#", Math.PI.ToString());
+
+            // Cycle through all the brackets following BEDMAS order and replace them with their solved version
+            while (true)
+            {
+                var bracketEquation = bracketPattern.Match(eq);
+
+                if (bracketEquation.Length == 0)
+                    break;
+
+                eq = eq.Replace(bracketEquation.Value, Solve(bracketEquation.Value.Substring(1, bracketEquation.Length - 2)));
+            }
+            
+            // Call the local functions to evaluate the equation following BEDMAS order
+            eq = Sqrt(eq);
+            eq = Exponants(eq);
+            eq = DivMult(eq);
+            eq = AddSub(eq);
+
+            while (true)
+            {
+                if (bracketPattern.Matches(eq).Count > 0)
+                    eq = Solve(eq);
+                else
+                    break;
+            }
+
+            return eq;
+        }
+
+        /// <summary>
+        /// Solves the square roots in the given equation.
+        /// </summary>
+        /// <param name="eq">Equation to solve.</param>
+        private static string Sqrt(string eq)
+        {
+            //var re = new Regex(@"@\((?<num>\d+(\.\d+)?)\)");
+            var sqrtRexex = new Regex(@"@\((?<eq>\d+(\.\d+)?([\^@/\*\+\-0-9.]+)?)\)");
+            var matches = sqrtRexex.Matches(eq);
+
+            foreach (Match match in matches)
+            {
+                //eq = eq.Replace(match.Value, Solve(match.Groups["eq"].Value));
+
+                var num = double.Parse(Solve(match.Groups["eq"].Value));
+                eq = eq.Replace(match.Value, Math.Sqrt(num).ToString());
+            }
+
+            while (true)
+            {
+                if (sqrtRexex.Matches(eq).Count > 0)
+                    eq = Sqrt(eq);
+                else
+                    break;
+            }
+
+            return eq;
+        }
+
+        /// <summary>
+        /// Solves the exponents in the given equation.
+        /// </summary>
+        /// <param name="eq">Equation to solve.</param>
+        private static string Exponants(string eq)
+        {
+            var exponantRegex = new Regex(@"(?<num1>\d+(\.\d+)?)\^(?<num2>\d+(\.\d+)?)");
+            var matches = exponantRegex.Matches(eq);
+
+            foreach (Match match in matches)
+            {
+                var groups = match.Groups;
+
+                var nums = new double[] { double.Parse(groups["num1"].Value), double.Parse(groups["num2"].Value) };
+                eq = eq.Replace(match.ToString(), Math.Pow(nums[0], nums[1]).ToString());
+            }
+
+            while (true)
+            {
+                if (exponantRegex.Matches(eq).Count > 0)
+                    eq = Exponants(eq);
+                else
+                    break;
+            }
+
+            return eq;
+        }
+
+        /// <summary>
+        /// Solves the division and multiplication in the given equation.
+        /// </summary>
+        /// <param name="eq">Equation to solve.</param>
+        private static string DivMult(string eq)
+        {
+            var divMultRegex = new Regex(@"(?<num1>\d+(\.\d+)?)(?<op>[\*\/])(?<num2>\d+(\.\d+)?)");
+            var matches = divMultRegex.Matches(eq);
+
+            foreach (Match match in matches)
+            {
+                var groups = match.Groups;
+
+                var op = groups["op"].Value;
+                var nums = new double[] { double.Parse(groups["num1"].Value), double.Parse(groups["num2"].Value) };
+
+                // Handle divide by 0 errors
+                if (nums[1] == 0 && op == "/")
+                    throw new DivideByZeroException($"can\'t divide {nums[0]} by 0");
+
+                eq = eq.Replace(match.ToString(), op == "*" ?
+                    (nums[0] * nums[1]).ToString() :
+                    (nums[0] / nums[1]).ToString());
+            }
+
+            while (true)
+            {
+                if (divMultRegex.Matches(eq).Count > 0)
+                    eq = DivMult(eq);
+                else
+                    break;
+            }
+
+            return eq;
+        }
+
+        /// <summary>
+        /// Solves the addition and subtraction in the given equation.
+        /// </summary>
+        /// <param name="eq">Equation to solve.</param>
+        private static string AddSub(string eq)
+        {
+            var addSubRegex = new Regex(@"(?<num1>\d+(\.\d+)?)(?<op>[\+\-])(?<num2>\d+(\.\d+)?)");
+            var matches = addSubRegex.Matches(eq);
+
+            foreach (Match match in matches)
+            {
+                var groups = match.Groups;
+
+                var op = groups["op"].Value;
+                var nums = new double[] { double.Parse(groups["num1"].Value), double.Parse(groups["num2"].Value) };
+
+                eq = eq.Replace(match.ToString(), op == "+" ?
+                    (nums[0] + nums[1]).ToString() :
+                    (nums[0] - nums[1]).ToString());
+            }
+
+            while (true)
+            {
+                if (addSubRegex.Matches(eq).Count > 0)
+                    eq = AddSub(eq);
+                else
+                    break;
+            }
 
             return eq;
         }
